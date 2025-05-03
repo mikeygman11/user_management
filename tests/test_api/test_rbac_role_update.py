353e75a56d6ba050f079a6fa4d779a1cc13bc0d2
mikeygman11 <mgalanaugh15@gmail.com>
2025-05-03 14:33:30 -0400

    tests for admin updating user to mgr and prohibiting non managers

---
+++ tests/test_api/test_rbac_role_update.py
@@ -0,0 +1,29 @@
+import pytest
+from httpx import AsyncClient
+from uuid import UUID
+from app.models.user_model import UserRole
+from app.schemas.user_schemas import UserCreate
+from app.main import app
+
+@pytest.mark.asyncio
+async def test_admin_can_update_user_role(admin_token, create_user, async_client: AsyncClient):
+    user = await create_user(role=UserRole.AUTHENTICATED)
+    res = await async_client.put(
+        f"/users/{user.id}/role",
+        headers={"Authorization": f"Bearer {admin_token}"},
+        json={"new_role": "MANAGER"}
+    )
+    assert res.status_code == 200
+    assert f"User role updated to MANAGER" in res.json()["message"]
+
+
+@pytest.mark.asyncio
+async def test_non_admin_cannot_update_user_role(user_token, create_user, async_client: AsyncClient):
+    user = await create_user(role=UserRole.AUTHENTICATED)
+    res = await async_client.put(
+        f"/users/{user.id}/role",
+        headers={"Authorization": f"Bearer {user_token}"},
+        json={"new_role": "ADMIN"}
+    )
+    assert res.status_code == 403
+    assert res.json()["detail"] == "Operation not permitted"
